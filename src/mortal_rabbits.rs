be29a5b6a95c18c30b7c517e3557d979a2f9e8c2
Andreas Arnold <andreas@featureenvy.com>
2015-02-24 18:50:13 +0100

    Another try with mortal rabbits.

--- src/mortal_rabbits.rs
+++ src/mortal_rabbits.rs
@@ -1,22 +1,26 @@
 // each pair produces one offspring.
 pub fn run(generations: u32, mortality: u32) -> f64 {
-    let result = (2..generations).fold(vec![1f64, 1f64], |mut data, _| {
-        let mature_rabbits = data[data.len()-2];
-        let child_rabbits = data[data.len()-1] - mature_rabbits;
-
-        let dying_rabbits = if data.len() >= (mortality + 1) as usize {
-            data[data.len()-(mortality + 1) as usize]
+    //println!("1\t0\t0\n0\t1\t0");
+    let result = (2..generations).fold(vec![(1f64,0f64,0f64), (0f64,1f64,0f64)], |mut data, _| {
+        let child_rabbits = data[data.len()-1].1;
+        let mature_rabbits = data[data.len()-1].0 + data[data.len()-1].1;
+        let dying_rabbits = if data.len() >= mortality as usize {
+            data[data.len()-mortality as usize].0
         } else {
             0f64
         };
-        let procreating_rabbits = mature_rabbits - dying_rabbits;
 
-        data.push(child_rabbits + procreating_rabbits * 2f64);
+        //println!("{}\t{}\t{}", child_rabbits, mature_rabbits - dying_rabbits, dying_rabbits);
+
+        let res = (child_rabbits, mature_rabbits - dying_rabbits, dying_rabbits);
+        data.push(res);
 
         data
     });
 
-    *result.last().expect("could not calculate the offsprings.")
+    let last = *result.last().expect("could not calculate the offsprings.");
+
+    last.1 + last.0
 }
 
 #[cfg(test)]
